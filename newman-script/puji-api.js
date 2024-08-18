812c7af004788cb51a131ff689f98df449e5e98c
Puji <xsripujiastuti@gmail.com>
2024-08-18 21:52:27 +0700

    add collection (using env)

--- newman-script/puji-api.js
+++ newman-script/puji-api.js
@@ -1,7 +1,7 @@
 const newman = require("newman");
 
 newman.run({
-  collection: "json-collection/puji.postman_collection.json",
+  collection: "json-collection/puji-using-env.postman_collection.json",
   environment: "json-env/puji.postman_environment.json",
   reporters: ["cli", "htmlextra"],
 });
